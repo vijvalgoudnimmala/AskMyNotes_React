3d5ab370de2ebbc53c4b4f24a92be1c6579bd930
vijvalgoudnimmala <vijvalme@gmail.com>
2026-08-13 12:20:22 +0530

    Fix Render backend Vite host configuration

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,11 @@ app = FastAPI(
 # Since these are different origins, CORS permission is required.
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://frontend-jiff.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
