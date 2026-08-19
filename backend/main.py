d3a2ac3fdc9851721fee52f2932fd41e7090e4e2
Vijvalgoudnimmala <vijvalme@gmail.com>
2026-08-19 11:28:48 +0530

    Final Updated files

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://frontend-askmynotes.onrender.com",
+        "https://priyanka-frontend.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
